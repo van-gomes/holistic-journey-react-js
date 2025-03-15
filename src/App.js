1a6c325fd93c9a9bb39fcbb95a7a18e87cfee30c
van-gomes <nninha.gomes@gmail.com>
2025-03-15 17:29:06 -0300

    Fail instructors

--- src/App.js
+++ src/App.js
@@ -45,8 +45,77 @@ export const App = () => {
       secondaryColor: "#FFEEDF",
     },
   ];
-
-  const [instructors, setInstructors] = useState([]);
+  
+  const initial = [
+    {
+      name: 'JULIANA AMOASEI',
+      description: 'Guia no caminho do autoconhecimento e da transformação interior.',
+      image: 'https://i.pinimg.com/736x/3e/95/69/3e95690f0966ea71b42733245ac4d285.jpg',
+      selectedMeditation: meditations[0].meditationName
+    },
+    {
+      name: 'DANIEL ARTINE',
+      description: 'Facilitador do despertar espiritual e do equilíbrio energético.',
+      image: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+      selectedMeditation: meditations[1].meditationName
+    },
+    {
+      name: 'Lily LIMA',
+      description: 'Conector de almas por meio da sabedoria e harmonia interior.',
+      image: 'https://i.pinimg.com/736x/e7/61/94/e761942a6f9a68383e3ac022b6e682af.jpg',
+      selectedMeditation: meditations[2].meditationName
+    },
+    {
+      name: 'Lily LIMA',
+      description: 'Conector de almas por meio da sabedoria e harmonia interior.',
+      image: 'https://i.pinimg.com/736x/9e/ab/6e/9eab6ea10417baf280d31c67f26b9b8c.jpg',
+      selectedMeditation: meditations[2].meditationName
+    },
+    {
+      name: 'PAULO SILVEIRA',
+      description: 'Mensageiro da paz e do propósito, guiando jornadas espirituais.',
+      image: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+      selectedMeditation: meditations[3].meditationName
+    },
+    {
+      name: 'PAULO SILVEIRA',
+      description: 'Mensageiro da paz e do propósito, guiando jornadas espirituais.',
+      image: 'https://i.pinimg.com/736x/33/4c/71/334c71f86e4aaefb3d903651b3a03f44.jpg',
+      selectedMeditation: meditations[3].meditationName
+    },
+    {
+      name: 'PAULO SILVEIRA',
+      description: 'Mensageiro da paz e do propósito, guiando jornadas espirituais.',
+      image: 'https://i.pinimg.com/736x/13/98/c3/1398c356929aba315aed0d69d3f5472c.jpg',
+      selectedMeditation: meditations[3].meditationName
+    },
+    {
+      name: 'PAULO SILVEIRA',
+      description: 'Mensageiro da paz e do propósito, guiando jornadas espirituais.',
+      image: 'https://i.pinimg.com/736x/fd/a6/19/fda6197cdabede6e23ceef0e92bef0d7.jpg',
+      selectedMeditation: meditations[3].meditationName
+    },
+    {
+      name: 'PAULO SILVEIRA',
+      description: 'Mensageiro da paz e do propósito, guiando jornadas espirituais.',
+      image: 'https://i.pinimg.com/736x/c2/a0/76/c2a0765a7c73a9b4d0764da3cebe406e.jpg',
+      selectedMeditation: meditations[3].meditationName
+    },
+    {
+      name: 'PAULO SILVEIRA',
+      description: 'Mensageiro da paz e do propósito, guiando jornadas espirituais.',
+      image: 'https://i.pinimg.com/736x/c2/a0/76/c2a0765a7c73a9b4d0764da3cebe406e.jpg',
+      selectedMeditation: meditations[3].meditationName
+    },
+    {
+      name: 'Isabela Oliveira',
+      description: 'Mensageira da paz e do propósito, guiando jornadas espirituais.',
+      image: 'https://i.pinimg.com/736x/6b/c8/28/6bc828111bf2c28d11319bcee4fdf11c.jpg',
+      selectedMeditation: meditations[3].meditationName
+    }
+  ]
+  
+  const [instructors, setInstructors] = useState(initial);
 
   const onNewInstructorAdded = (instructor) => {
     setInstructors([...instructors, instructor]);
